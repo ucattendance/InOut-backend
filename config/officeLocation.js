aa7ae62e1699342fee7b70961931e92d35f36f1f
SivaSankaraPandian3 <sivasankarapandian3@gmail.com>
2026-06-08 09:34:10 +0530

    location

--- config/officeLocation.js
+++ config/officeLocation.js
@@ -11,5 +11,11 @@ module.exports = [
     latitude: 12.9912597,
     longitude: 80.2201616,
     radiusMeters: 400, // Acceptable distance from Office 2
+  },
+  {
+    name: "Tirunelveli",
+    latitude: 8.6988125,
+    longitude: 77.7269375,
+    radiusMeters: 350, // Fab Sapphire Towers (MPXG+GQ)
   }
 ];
